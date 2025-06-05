9a257a2f6c5ea9106f8c643c16301e2e8f69f4f0
gustavnybergs <Gustavnybergs@gmail.com>
2025-06-05 12:34:40 +0200

    Initial project setup

---
+++ src/main/java/se/secure/springapp/securespringapp/SecureSpringAppApplication.java
@@ -0,0 +1,13 @@
+package se.secure.springapp.securespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/secure/springapp/securespringapp/SecureSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.secure.springapp.securespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
